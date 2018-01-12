40915825577b1b7c06e99e5f1f2010f4cad66449
Tatyana Scotce <tscotce@gmail.com>
2018-01-12 23:18:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz (number)
-  if number % 3 == 0 
-    "Fizz"
-  else number % 5 == 0 
-      "Buzz"
-  else number % 3 == 0 && number % 5 == 0 
+  if number % 3 == 0 && number % 5 == 0 
         "Fizzbuzz"
+  elsif number % 3 == 0 
+    "Fizz"
+  elsif number % 5 == 0 
+      "Buzz"
   end
 end
